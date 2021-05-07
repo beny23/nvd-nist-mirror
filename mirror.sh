7377f973a3231eb4b4fb78c2cedbc17a25600e6b
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:56:50 +0100

    use original timestamps for files

--- mirror.sh
+++ mirror.sh
@@ -3,12 +3,12 @@
 mkdir -p target/feeds/json/cve/1.1
 (
   cd target/feeds/json/cve/1.1
-  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta -O --fail
-  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz -O --fail
+  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta -OR --fail
+  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz -OR --fail
   for year in $(seq 2002 `date +%Y`)
   do
-    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.meta -O --fail
-    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.json.gz -O --fail
+    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.meta -OR --fail
+    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.json.gz -OR --fail
   done
 )
 
