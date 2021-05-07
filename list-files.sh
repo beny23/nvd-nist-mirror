0c16c46109fdbd2381afa954cd9f0945381b169a
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:35:17 +0100

    fix hyperlinking

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body>'
-  find -maxdepth 1 -type f -printf '%t %s %p\n' | grep -v index.html | sed 's#^[^ ]*#<a href="&">&</a>#' | sed '#$#<br>#'
+  find -maxdepth 1 -type f -printf '%p %s %t\n' | grep -v index.html | sed 's#^[^ ]*#<a href="&">&</a>#' | sed '#$#<br>#'
   echo '</body></html>'
 ) > $1/index.html
