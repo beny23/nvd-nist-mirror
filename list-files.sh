3e40e022962ce863566ffa058466abf12113772c
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 02:37:37 +0100

    Skip index.html from file listings

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body>'
-  ls -1 $1 | sed 's/^.*/<a href="&">&<\/a><br\/>/'
+  ls -1 $1 | grep -v index.html | sed 's/^.*/<a href="&">&<\/a><br\/>/'
   echo '</body></html>'
 ) > $1/index.html
