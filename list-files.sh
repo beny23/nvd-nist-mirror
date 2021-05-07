0ea535354f9a7ec4c23298bffb764ef84816c7a0
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:38:32 +0100

    fix directory

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body>'
-  find -maxdepth 1 -type f -printf '%p %s %t\n' | grep -v index.html | sed 's#^[^ ]*#<a href="&">&</a>#' | sed '#$#<br>#'
+  find $1 -maxdepth 1 -type f -printf '%p %s %t\n' | grep -v index.html | sed 's#^[^ ]*#<a href="&">&</a>#' | sed '#$#<br>#'
   echo '</body></html>'
 ) > $1/index.html
