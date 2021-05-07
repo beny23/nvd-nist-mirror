77ed1cc769c746a86c7dbd068782563cd872287c
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 04:02:00 +0100

    better timestamps

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body><table>'
-  ls -lh --time-style="+%Y-%m-%d %H:%M:%S" $1 | grep -v total | grep -v index.html | awk '{ printf("<tr><td> <a href=\"%s\">%s</a></td> <td>%s</td> <td> %s %s %s </td></tr>\n", $9, $9, $5, $6, $7, $8); }'
+  ls -lh --time-style="+%Y-%m-%d %H:%M:%S" $1 | grep -v total | grep -v index.html | awk '{ printf("<tr><td> <a href=\"%s\">%s</a></td> <td>%s</td> <td> %s %s </td></tr>\n", $8, $8, $5, $6, $7); }'
   echo '</table></body></html>'
 ) > $1/index.html
