6e507485ca706990d603986de59b247622f66545
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:31:04 +0100

    slightly better file list

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body>'
-  ls -l $1 | grep -v index.html | sed 's/^.*/<a href="&">&<\/a><br\/>/'
+  find -maxdepth 1 -type f -printf '%t %s %p\n' | grep -v index.html | sed 's/^[^ ]*/<a href="&">&<\/a><br\/>/'
   echo '</body></html>'
 ) > $1/index.html
