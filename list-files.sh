2858890018e8c9e3ac30ffc75bb7fc3a157b8eff
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:50:14 +0100

    fix sort order

--- list-files.sh
+++ list-files.sh
@@ -2,6 +2,6 @@
 
 (
   echo '<html><body><table>'
-  ls -ltrh $1 | grep -v total | awk '{ printf("<td> <a href=\"%s\">%s</a></td> <td>%s</td> <td> %s %s %s </td>\n", $9, $9, $5, $6, $7, $8); }'
+  ls -lh $1 | grep -v total | awk '{ printf("<td> <a href=\"%s\">%s</a></td> <td>%s</td> <td> %s %s %s </td>\n", $9, $9, $5, $6, $7, $8); }'
   echo '</table></body></html>'
 ) > $1/index.html
