14e0e74f862f6c9358ca130b1e50a38d4014ec13
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 02:35:03 +0100

    Create file listings

--- mirror.sh
+++ mirror.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-set -ex
+
 mkdir -p target/feeds/json/cve/1.1
-cd target/feeds/json/cve/1.1
-curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta -O --fail
-curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz -O --fail
-for year in $(seq 2002 `date +%Y`)
-do
-	curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.meta -O --fail
-	curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.json.gz -O --fail
-done
+(
+  cd target/feeds/json/cve/1.1
+  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.meta -O --fail
+  curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-modified.json.gz -O --fail
+  for year in $(seq 2002 `date +%Y`)
+  do
+    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.meta -O --fail
+    curl https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-$year.json.gz -O --fail
+  done
+)
 
 find target -type d -exec bash list-files.sh {} \;
