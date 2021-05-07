d0b16a131df12e9ee51b4eb665b37a0db7b8b727
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-05-07 03:49:28 +0100

    fix directory

--- list-files.sh
+++ list-files.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-DIR=$1
-
 (
-  echo '<html><body>'
-  find $DIR -maxdepth 1 -type f -printf '%p %s %t\n' | grep -v index.html | sed 's#^[^ ]*#<a href="&">&</a>#' | sed '#$#<br>#'
-  echo '</body></html>'
-) > $DIR/index.html
+  echo '<html><body><table>'
+  ls -ltrh $1 | grep -v total | awk '{ printf("<td> <a href=\"%s\">%s</a></td> <td>%s</td> <td> %s %s %s </td>\n", $9, $9, $5, $6, $7, $8); }'
+  echo '</table></body></html>'
+) > $1/index.html
